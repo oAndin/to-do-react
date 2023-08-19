4ca0906c34d687da7ca97e2e9a68f8aaeddbb208
oAndin <00andherson@gmail.com>
2023-08-19 00:28:18 -0300

    Read me update

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple To Do React project
+
+## First it will be a working one, later on, i will try to add some features, like stages of activities, moving towards a more complex application!
--- src/Components/DarkMode/index.jsx
+++ src/Components/DarkMode/index.jsx
@@ -2,16 +2,17 @@ import { useState } from "react";
 import 'style.css';
 
 const DarkMode = () => {
-
+  // get the body element to switch to dark mode color
   let body = document.querySelector('body');
 
+
+  // Save last session theme so it doesnt change on reload
   // setDarkMode = () => {
   //   if (localStorage) localStorage['dark-mode'] ? body?.classList.add("dark") : null;
   // }
 
-
   if (darkMode) {
-    setDarkMode
+    body.
   };
 
   const [darkMode, setDarkMode] = useState(true);
