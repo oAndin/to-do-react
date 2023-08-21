3646a8a8d1975c4511096163c20b3ce4bc1abb3d
oAndin <00andherson@gmail.com>
2023-08-21 03:35:24 -0300

    Test with LIs 0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { MdDelete } from 'react-icons/md'
+
 import './App.css';
 
 const App = () => {
@@ -6,6 +8,13 @@ const App = () => {
   const ESC_KEY = 27;
   const ENTER_KEY = 13;
 
+  const initialTodos = [
+    { id: 1, title: "Estudar React", checked: false },
+    { id: 2, title: "Estudar Inglês", checked: true },
+    { id: 3, title: "Tocar guitarra", checked: false },
+  ];
+
+  const [todos,] = useState(initialTodos);
   const [value, setValue] = useState('');
 
   const erase = () => {
@@ -45,8 +54,20 @@ const App = () => {
           onChange={onChange}
           onKeyDown={onKeyDown}
         />
+        <ul className='todo-list'>
+          {todos.map((todo) => (
+            <li key={todo.id.toString} >
+              <span className='todo'>
+                {todo.title}
+              </span>
+              <button className='remove' type='button'>
+                <MdDelete size={32} />
+              </button>
+            </li>
+          ))}
+        </ul>
       </section>
-    </ section>
+    </ section >
   );
 };
 
