27e5678b1931bd6b9f4a0d1111b02e918cc6956a
oAndin <00andherson@gmail.com>
2023-08-22 20:40:17 -0300

    Trying styled components

--- src/Components/DarkMode/index.jsx
+++ src/Components/DarkMode/index.jsx
@@ -1,18 +1,33 @@
 import { useState } from "react";
 
+import { styled } from "styled-components";
+
+let color;
+
+
 const DarkMode = () => {
+
+  const [darkMode, setDarkMode] = useState(true);
   // get the body element to switch to dark mode color
   let body = document.querySelector('body');
 
-  const [darkMode, setDarkMode] = useState(false);
+  if (darkMode === true) {
+    body.style.backgroundColor = "#1b1b1b";
+    color = "#d9d9d9"
+  }
+  else {
+    body.style.backgroundColor = "#d9d9d9"
+    color = "#1b1b1b"
+  }
 
-
-    if (darkMode === true) {
-      body.style.backgroundColor = "#1b1b1b"
-    }
-    else {
-      body.style.backgroundColor = "#d9d9d9"
-    }
+  const ModeBtn = styled.button`
+  position: absolute;
+  margin: 8px;
+  top:0%;
+  right:0%;
+  padding: 8px 8px;
+  border: 1px solid ${color};
+  `;
 
   // Save last session theme so it doesnt change on reload
   //   if (localStorage) localStorage['dark-mode'] ? body?.classList.add("dark") : null;
@@ -20,9 +35,11 @@ const DarkMode = () => {
 
   return (
     <>
-      <button className="mode" onClick={() => setDarkMode(!darkMode)}>AAAAA</button>
+      <ModeBtn onClick={() => setDarkMode(!darkMode)}>Toggle Mode</ModeBtn>
     </>
   )
-}
+
+};
+
 
 export default DarkMode;
